8c363fbe6527645aaeea4311419f0f43e4d911bf
Operel <oriperelman@gmail.com>
2020-09-19 16:13:51 +0300

    contact form basic functionality and design

--- src/components/sections/footer/footer.tsx
+++ src/components/sections/footer/footer.tsx
@@ -1,4 +1,10 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
+
+const initialFormState = {
+  name: '',
+  email: '',
+  message: ''
+}
 
 @Component({
   tag: 'contact-footer',
@@ -6,8 +12,31 @@ import { Component, h } from '@stencil/core';
   // shadow: true
 })
 export class Footer {
+  @State() name: string;
+  @State() email: string;
+  @State() message: string;
+
+  constructor() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+
+  handleInputChange(e: Event): void {
+    this[(e.target as HTMLInputElement).name] = (e.target as HTMLInputElement).value;
+  }
+
+  handleSubmitForm(e: Event): void {
+    e.preventDefault();
+    console.log('submit form: ', this.name, this.email, this.message)
+
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
 
   render() {
+    const { name, email, message } = this;
     return (
       <footer>
         <div class="container contact">
@@ -17,20 +46,34 @@ export class Footer {
             <p>bla bla bla bla bla bla bla ddaddaddad</p>
           </div>
 
-          <form style={{ zIndex: '101' }}>
+          <form>
             <ion-item>
               <ion-label position="floating">Full Name</ion-label>
-              <ion-input type="text" name="fullName"></ion-input>
+              <ion-input
+                type="text"
+                name="name"
+                value={name}
+                onInput={(e: Event) => {this.handleInputChange(e)}}
+              ></ion-input>
             </ion-item>
             <ion-item>
               <ion-label position="floating">Email</ion-label>
-              <ion-input type="email" name="email"></ion-input>
+              <ion-input
+                type="email"
+                name="email"
+                value={email}
+                onInput={(e: Event) => {this.handleInputChange(e)}}
+              ></ion-input>
             </ion-item>
             <ion-item>
               <ion-label position="floating">Message</ion-label>
-              <ion-textarea name="message"></ion-textarea>
+              <ion-textarea
+                name="message"
+                value={message}
+                onInput={(e: Event) => {this.handleInputChange(e)}}
+              ></ion-textarea>
             </ion-item>
-            <ion-button>Submit</ion-button>
+            <ion-button onClick={(e: Event) => this.handleSubmitForm(e)}>Submit</ion-button>
           </form>
         
         </div>
