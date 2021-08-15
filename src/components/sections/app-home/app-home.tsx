6ab7f48f89f2cca93596ae97b64198defcaf0847
Operel <oriperelman@gmail.com>
2021-08-15 15:24:05 +0300

    test home svg bg

--- src/components/sections/app-home/app-home.tsx
+++ src/components/sections/app-home/app-home.tsx
@@ -11,19 +11,24 @@ export class AppHome {
   render() {
     return (
       <section class="app-home">
-        <section class={`home-background`} />
-        <div class="container">
-          <parallax-el animeClass={this.animeClass} on={5} over={-60} under={0}>
-            <h2>Welcome</h2>
+        {/*<div class="home-background">*/}
+          <div class="home-container">
+            <parallax-el animeClass={this.animeClass} on={-5} over={-60} under={0}>
+              <h1>Welcome</h1>
+            </parallax-el>
+            <parallax-el animeClass={this.animeClass} on={-5} over={-90} under={0}>
+              <h3>This is my example tagline. bla bla bla...</h3>
+            </parallax-el>
+          </div>
+
+          <parallax-el animeClass={this.animeClass} on={-60} over={60} under={0}>
+            <img src="../../../assets/test1.svg" alt="hex" />
           </parallax-el>
-          <parallax-el animeClass={this.animeClass} on={5} over={-90} under={0}>
-            <h3>This is my example tagline. bla bla bla...</h3>
-          </parallax-el>
-        </div>
 
         {/* <growing-tri color={`#226765`} cp={this.cp} page={0} on={0} over={30} under={0} growDir={`left`} /> */}
-        <rotating-tri color="#226765" side="left" animeClass={this.animeClass} height={30} origin="bottom right" on={-10} under={30} over={0} />
+        {/*<rotating-tri color="#226765" side="left" animeClass={this.animeClass} height={30} origin="bottom right" on={-10} under={30} over={0} />*/}
 
+        {/*</div>*/}
       </section>
     );
   }
